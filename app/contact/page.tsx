a565fb910adf83d852d76242a410c70800faeaad
chizkuhhh <chizkuhhh22@gmail.com>
2026-08-25 19:06:23 +0800

    update layout & buttons

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,11 +5,10 @@ import BackButton from "@/components/backButton";
 export default function ContactPage() {
   return (
     <div className="flex flex-col items-center px-5 pt-32 pb-12 md:px-8 bg-black-haze-100 min-h-screen">
+      <div className="mb-6 self-start">
+        <BackButton />
+      </div>
       <div className="w-full max-w-md">
-        <div className="mb-6">
-          <BackButton />
-        </div>
-
         <h1 className="text-2xl font-bold text-mine-shaft-900 mb-2 text-center">
           Contact Us
         </h1>
@@ -17,26 +16,24 @@ export default function ContactPage() {
           Message us to order directly.
         </p>
 
-        <div className="flex flex-col gap-3 items-center">
+        <div className="flex flex-col gap-3 w-auto text-md items-center">
           <Button
-            render={<a href="https://www.facebook.com/KantoSportsPHL" target="_blank" rel="noopener noreferrer" />}
             className="rounded-full h-12 bg-mine-shaft-800 hover:bg-mine-shaft-900 px-4 w-full max-w-xs"
           >
-            <span className="flex gap-2 items-center justify-center">
+            <a href="https://www.facebook.com/KantoSportsPHL" target="_blank" rel="noopener noreferrer" className="flex gap-2 items-center justify-center">
               <SiFacebook className="text-[#1877F2]"/>
               <p>Message us on <span className="font-bold">Facebook</span></p>
-            </span>
+            </a>
           </Button>
 
           <Button
-            render={<a href="https://www.instagram.com/kantophl?igsi=MWx4Y2tuNmVkOXRncw%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer" />}
             variant="outline"
             className="rounded-full h-12 border-mine-shaft-800 bg-black-haze-100 px-4 w-full max-w-xs"
           >
-            <span className="flex gap-2 items-center justify-center">
+            <a href="https://www.instagram.com/kantophl?igsi=MWx4Y2tuNmVkOXRncw%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer" className="flex gap-2 items-center justify-center">
               <SiInstagram className="text-[#E4405F]"/>
               <p>Message us on <span className="font-bold">Instagram</span></p>
-            </span>
+            </a>
           </Button>
         </div>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col pb-5">
         <Header />
         {children}
       </body>
